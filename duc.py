ed79106f86c09b780920d2dc49b29fecae8c31fc
Thalles Koester <thallesdella@gmail.com>
2018-11-29 14:35:31 -0200

    Secure Update

--- duc.py
+++ duc.py
@@ -4,7 +4,8 @@ import time as t
 import sys as s
 import re
 
-zona = 'casap.bunana.com.br'
+zona = '%Nome_da_entrada%' # casa.bunana.com.br
+zone_id = '%Id_da_zona%' # 970c55a5e3c70eb5703b582e9d939e19
 
 def ipType(ip):
     ip = str(ip)
@@ -40,24 +41,24 @@ if __name__ == "__main__":
                     t.sleep(5)
 		
             headers = {
-                'X-Auth-Email' : 'thallesdella@gmail.com',
-                'X-Auth-Key' : 'c46b7425ea00e9789e59897234df1529edc95',
+                'X-Auth-Email' : '%Email%', # teste@gmail.com
+                'X-Auth-Key' : '%Auth_key%', # c46b7425ea00e9789e59897234df1529edc96
                 'Content-Type' : 'application/json'
             }
 		
             # Obtem informações da zona DNS
             print('Obtendo Informações da Zona!')
-            url = 'https://api.cloudflare.com/client/v4/zones/970c55a5e3c70eb5703b582e9d939e18/dns_records?name={}'.format(zona)
+            url = 'https://api.cloudflare.com/client/v4/zones/{}/dns_records?name={}'.format(zone_id, zona)
             conn = r.get(url, headers=headers)
             if conn.status_code == r.codes.ok:
                 json = conn.json()['result'][0]
-                zone_id = json['id']
-                zone_ip = json['content']
+                record_id = json['id']
+                record_ip = json['content']
 		
             # Atualiza IP na zona
-            if ip != zone_ip:
+            if ip != record_ip:
                 print('Atualizando Zona!')
-                url = 'https://api.cloudflare.com/client/v4/zones/970c55a5e3c70eb5703b582e9d939e18/dns_records/{}'.format(zone_id)
+                url = 'https://api.cloudflare.com/client/v4/zones//dns_records/{}'.format(zone_id, record_id)
                 payload = {
                     'type' : ipType(ip),
                     'name' : zona,
